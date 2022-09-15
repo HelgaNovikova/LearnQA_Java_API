8085254ac41b467a8f82b361f9e6b40769813649
HelgaNovikova <helpotaki@rambler.ru>
2022-09-15 15:21:21 -0400

    Ex4 get

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,12 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
     public void helloWorldTest(){
-        System.out.println("Hello from Olga");
+        Response response = RestAssured.get("https://playground.learnqa.ru/api/get_text").andReturn();
+        response.prettyPrint();
     }
 }
