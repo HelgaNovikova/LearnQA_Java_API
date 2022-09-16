cee53e888d078d0c2204e6e842ae3ae49a36f026
HelgaNovikova <helpotaki@rambler.ru>
2022-09-16 12:48:35 -0400

    Ex5

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,12 +1,33 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class HelloWorldTest {
 
     @Test
-    public void helloWorldTest(){
-        Response response = RestAssured.get("https://playground.learnqa.ru/api/get_text").andReturn();
+    public void helloYou(){
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Olia");
+        Response response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello").andReturn();
         response.prettyPrint();
     }
+
+    @Test
+    public void ex5Test(){
+        JsonPath response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                        .jsonPath();
+        String message = response.get("messages[1].message");
+        System.out.println(message);
+    }
+
 }
