bd1d1581fb057836ff548a319f09e1ca322a8443
HelgaNovikova <helpotaki@rambler.ru>
2022-09-22 12:28:04 -0400

    Ex11

--- src/test/java/Module3tests.java
+++ src/test/java/Module3tests.java
@@ -14,7 +14,18 @@ public class Module3tests {
 
     @ParameterizedTest
     @ValueSource(strings = {"dkgdfg", "erjfn4569dkrudn", "erjfn4569dkrudn3"})
-    public void ex10Test(String str){
+    public void ex10Test(String str) {
         assertTrue(str.length() > 15, "length of the string is less then 15 symbols");
+    }
+
+    @Test
+    public void ex11Test() {
+        Response requestForCookies = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        assertEquals("hw_value", requestForCookies.getCookie("HomeWork"), "cookie isn't correct");
     }
 }
